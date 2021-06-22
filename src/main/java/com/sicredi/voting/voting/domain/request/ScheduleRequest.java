737a0164d3ee2aea93d18be07b72ca23a3c8d3c3
Matheus C Pereira <mpereira@grupowos.com.br>
2021-06-22 06:30:35 -0400

    Start a mapper of Schedule

---
+++ src/main/java/com/sicredi/voting/voting/domain/request/ScheduleRequest.java
@@ -0,0 +1,42 @@
+package com.sicredi.voting.voting.domain.request;
+
+import com.google.common.base.Objects;
+
+import java.io.Serializable;
+
+public class ScheduleRequest implements Serializable{
+    private static final long serialVersionUID = 4011556253679507550L;
+
+    private String subject;
+
+    private String description;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ScheduleRequest)) return false;
+        ScheduleRequest that = (ScheduleRequest) o;
+        return Objects.equal(getSubject(), that.getSubject()) && Objects.equal(getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getSubject(), getDescription());
+    }
+}
